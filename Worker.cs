f94bee3a18ecdd65b31ffc2f679bae0539034eeb
JoelMiller74 <joel.miller@pc-pipeline.com>
2018-04-01 21:32:25 -0500

    initial commit

--- Worker.cs
+++ Worker.cs
@@ -3,16 +3,25 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 
 class Worker
 {
     public static void Main()
     {
-        var factory = new ConnectionFactory() { HostName = "localhost" };
+        //https://leonidius2010.wordpress.com/2017/09/21/reading-appsettings-in-net-core-2-console-application/
+        IConfiguration config = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json", true, true)
+          .Build();
+        string RabbitMQHost = config["RabbitMQHost"];
+        string RabbitMQQueueName = config["RabbitMQQueueName"];
+
+        //https://www.rabbitmq.com/tutorials/tutorial-two-dotnet.html
+        var factory = new ConnectionFactory() { HostName = RabbitMQHost };
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: RabbitMQQueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
             channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
             Console.WriteLine(" [*] Waiting for messages..");
             var consumer = new EventingBasicConsumer(channel);
@@ -21,12 +30,12 @@ class Worker
                 var body = ea.Body;
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine(" [x] Received {0}", message);
-                int dots = message.Split('.').Length - 1;
-                Thread.Sleep(dots * 1000);
-                Console.WriteLine(" [x] Done");
+                //int dots = message.Split('.').Length - 1;
+                //Thread.Sleep(dots * 1000);
+                Console.WriteLine(" [x] Done {0}", message);
                 channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
-            channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+            channel.BasicConsume(queue: RabbitMQQueueName, autoAck: false, consumer: consumer);
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
         }
